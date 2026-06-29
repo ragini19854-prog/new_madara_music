904f4b293dc65ce8a049987ee1d18c18d820045d
ragini19854-prog <ragini.19854@gmail.com>
2026-06-30 02:21:09 +0530

    🎵 Add MADARAMUSIC/utils/decorators/language.py

---
+++ MADARAMUSIC/utils/decorators/language.py
@@ -0,0 +1,69 @@
+# -----------------------------------------------
+# 🔸 MadaraMusic Project
+# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzShukla
+# -----------------------------------------------
+from MADARAMUSIC.misc import SUDOERS
+from MADARAMUSIC.utils.database import get_lang, is_maintenance
+from strings import get_string
+
+
+def language(mystic):
+    async def wrapper(_, message, **kwargs):
+        if await is_maintenance() is False:
+            if message.from_user.id not in SUDOERS:
+                return await message.reply_text(
+                    text=f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ <a href={SUPPORT_CHAT}>sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ</a> ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    disable_web_page_preview=True,
+                )
+        try:
+            await message.delete()
+        except:
+            pass
+
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
+
+
+def languageCB(mystic):
+    async def wrapper(_, CallbackQuery, **kwargs):
+        if await is_maintenance() is False:
+            if CallbackQuery.from_user.id not in SUDOERS:
+                return await CallbackQuery.answer(
+                    f"{app.mention} ɪs ᴜɴᴅᴇʀ ᴍᴀɪɴᴛᴇɴᴀɴᴄᴇ, ᴠɪsɪᴛ sᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ ғᴏʀ ᴋɴᴏᴡɪɴɢ ᴛʜᴇ ʀᴇᴀsᴏɴ.",
+                    show_alert=True,
+                )
+        try:
+            language = await get_lang(CallbackQuery.message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, CallbackQuery, language)
+
+    return wrapper
+
+
+def LanguageStart(mystic):
+    async def wrapper(_, message, **kwargs):
+        try:
+            language = await get_lang(message.chat.id)
+            language = get_string(language)
+        except:
+            language = get_string("en")
+        return await mystic(_, message, language)
+
+    return wrapper
