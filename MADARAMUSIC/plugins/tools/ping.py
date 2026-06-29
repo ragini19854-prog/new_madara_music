17c33f2e99ba43f6f1fed0058c0cc0cecf50b1e8
ragini19854-prog <ragini.19854@gmail.com>
2026-06-30 02:20:25 +0530

    🎵 Add MADARAMUSIC/plugins/tools/ping.py

---
+++ MADARAMUSIC/plugins/tools/ping.py
@@ -0,0 +1,42 @@
+# -----------------------------------------------
+# 🔸 MadaraMusic Project
+# 🔹 Developed & Maintained by: Shashank Shukla (https://github.com/itzshukla)
+# 📅 Copyright © 2022 – All Rights Reserved
+#
+# 📖 License:
+# This source code is open for educational and non-commercial use ONLY.
+# You are required to retain this credit in all copies or substantial portions of this file.
+# Commercial use, redistribution, or removal of this notice is strictly prohibited
+# without prior written permission from the author.
+#
+# ❤️ Made with dedication and love by ItzShukla
+# -----------------------------------------------
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import *
+from MADARAMUSIC import app
+from MADARAMUSIC.core.call import MADARA
+from MADARAMUSIC.utils import bot_sys_stats
+from MADARAMUSIC.utils.decorators.language import language
+from MADARAMUSIC.utils.inline import supp_markup
+from config import BANNED_USERS, MADARA_IMG
+import random
+
+
+@app.on_message(filters.command("ping", prefixes=["/"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    start = datetime.now()
+    response = await message.reply_photo(
+        random.choice(MADARA_IMG),
+        caption=_["ping_1"].format(app.mention),
+    )
+    pytgping = await MADARA.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
+        reply_markup=supp_markup(_),
+    )
