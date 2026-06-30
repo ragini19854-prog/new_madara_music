9935c1e6daee40aa7d4feffe288165eb06196b1c
ragini19854-prog <ragini.19854@gmail.com>
2026-06-30 07:49:42 +0530

    madara: update MADARAMUSIC/plugins/sudo/post.py

---
+++ MADARAMUSIC/plugins/sudo/post.py
@@ -0,0 +1,22 @@
+# ╔══════════════════════════════════════════════════╗
+# ║        🎵  M A D A R A  M U S I C  🎵           ║
+# ║  The Most Powerful Telegram Music Bot            ║
+# ║  Built with ❤️ for music lovers everywhere       ║
+# ╚══════════════════════════════════════════════════╝
+from pyrogram import Client, filters
+from MADARAMUSIC import app
+from config import OWNER_ID, BOT_USERNAME
+from pyrogram.types import Message
+
+
+@app.on_message(filters.command(["post"], prefixes=["/", "."]) & filters.user(OWNER_ID))
+async def copy_messages(_, message):
+
+    if message.reply_to_message:
+      
+        destination_group_id = -1001927107785
+ 
+
+        
+        await message.reply_to_message.copy(destination_group_id)
+        await message.reply("ᴘᴏsᴛ sᴜᴄᴄᴇssғᴜʟ ᴅᴏɴᴇ ")
