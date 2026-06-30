b341fabc96df1561e2b8320cc9c10d2978ebfce8
ragini19854-prog <ragini.19854@gmail.com>
2026-06-30 07:47:54 +0530

    madara: update MADARAMUSIC/plugins/admins/banall.py

---
+++ MADARAMUSIC/plugins/admins/banall.py
@@ -0,0 +1,33 @@
+# ╔══════════════════════════════════════════════════╗
+# ║        🎵  M A D A R A  M U S I C  🎵           ║
+# ║  The Most Powerful Telegram Music Bot            ║
+# ║  Built with ❤️ for music lovers everywhere       ║
+# ╚══════════════════════════════════════════════════╝
+from MADARAMUSIC import app
+from config import OWNER_ID
+from pyrogram import filters, enums
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from MADARAMUSIC.utils.Yukii_ban import admin_filter
+from MADARAMUSIC.misc import SUDOERS
+
+BOT_ID = app.me.id  # Corrected this line
+
+
+@app.on_message(filters.command("banall") & SUDOERS)
+async def ban_all(_, msg):
+    chat_id = msg.chat.id
+    bot = await app.get_chat_member(chat_id, BOT_ID)
+    bot_permission = bot.privileges.can_restrict_members == True
+    if bot_permission:
+        async for member in app.get_chat_members(chat_id):
+            try:
+                await app.ban_chat_member(chat_id, member.user.id)
+                await msg.reply_text(
+                    f"**‣ ᴇᴋ ᴏʀ ᴍᴀʀ ɢʏᴀ ᴍᴄ 🥺 .**\n\n➻ {member.user.mention}"
+                )
+            except Exception:
+                pass
+    else:
+        await msg.reply_text(
+            "ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs \n ᴏᴡɴᴇʀ ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴋᴇ sᴜᴅᴏ ʟᴇ ʟᴇ || @Kaito_3_2||"
+        )
